013d0c6daf52620c01be9513eda13509c2e9f6dc
ngohoang2110 <hoanggg2110@gmail.com>
2022-09-08 00:20:17 +0700

    Initial Project

---
+++ src/main/resources/db.migration/V20220902214100__create_ms_properties_table.sql
@@ -0,0 +1,16 @@
+set search_path to mhmarket;
+create extension if not exists pgcrypto;
+create sequence ms_properties_propertyid_seq increment 1 minvalue 1;
+
+DROP TABLE IF EXISTS ms_properties;
+CREATE TABLE ms_properties(
+                           propertyid integer primary key default nextval('ms_properties_propertyid_seq'::regclass),
+                           application varchar(50),
+                           profile varchar(50),
+                           label varchar(50),
+                           key varchar(100) not null,
+                           value varchar(100) not null,
+                          description text,
+                          createdat timestamp not null default now(),
+                          updatedat timestamp default now()
+)
