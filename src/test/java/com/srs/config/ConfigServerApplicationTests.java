e56848c4f8edeeea66cf6d48a6b1ddd3316361fe
ngohoang2110 <hoanggg2110@gmail.com>
2022-09-07 21:22:10 +0700

    Initial project

---
+++ src/test/java/com/srs/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srs.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
