62d1df3d23a075199083417427f570a59f49d3ee
BANVIENCORP\hoang.ngo <hoang.ngo@banvien.com.vn>
2022-09-17 15:26:42 +0700

    Config Kafka

--- src/main/resources/db.migration/V20220911122123__seed_market_service_properties_config.sql
+++ src/main/resources/db.migration/V20220911122123__seed_market_service_properties_config.sql
@@ -2,28 +2,28 @@ set search_path to mhmarket;
 
 DELETE
 FROM ms_properties
-WHERE application = 'account-service'
+WHERE application = 'market-service'
   AND profile = 'dev';
 
 -- Port
 INSERT INTO ms_properties (application, profile, label, key, value)
-VALUES ('account-service', 'dev', 'master', 'server.port', '8080');
+VALUES ('market-service', 'dev', 'master', 'server.port', '8081');
 INSERT INTO ms_properties (application, profile, label, key, value)
-VALUES ('account-service', 'dev', 'master', 'grpc.server.port', '6565');
+VALUES ('market-service', 'dev', 'master', 'grpc.server.port', '6566');
 
 -- Datasource
 INSERT INTO ms_properties (application, profile, label, key, value)
-VALUES ('account-service', 'dev', 'master', 'spring.datasource.url',
-        'jdbc:postgresql://localhost:5432/marketaccount');
+VALUES ('market-service', 'dev', 'master', 'spring.datasource.url',
+        'jdbc:postgresql://localhost:5432/marketmarket');
 INSERT INTO ms_properties (application, profile, label, key, value)
-VALUES ('account-service', 'dev', 'master', 'spring.datasource.username', 'postgres');
+VALUES ('market-service', 'dev', 'master', 'spring.datasource.username', 'postgres');
 INSERT INTO ms_properties (application, profile, label, key, value)
-VALUES ('account-service', 'dev', 'master', 'spring.datasource.password', '123456');
+VALUES ('market-service', 'dev', 'master', 'spring.datasource.password', '123456');
 INSERT INTO ms_properties (application, profile, label, key, value)
-VALUES ('account-service', 'dev', 'master', 'spring.datasource.hikari.schema', 'mhmarket');
+VALUES ('market-service', 'dev', 'master', 'spring.datasource.hikari.schema', 'mhmarket');
 
 INSERT INTO ms_properties (application, profile, label, key, value)
-VALUES ('account-service', 'dev', 'master', 'spring.jpa.properties.hibernate.default_schema', 'mhmarket');
+VALUES ('market-service', 'dev', 'master', 'spring.jpa.properties.hibernate.default_schema', 'mhmarket');
 -- INSERT INTO ms_properties (application, profile, label, key, value)
 -- VALUES ('account-service', 'dev', 'master', 'spring.jpa.properties.hibernate.metadata_builder_contributor',
 --         'com.banvien.emarket.account.config.QueryDslContributor');
@@ -36,6 +36,6 @@ VALUES ('account-service', 'dev', 'master', 'spring.jpa.properties.hibernate.def
 -- VALUES ('account-service', 'dev', 'master', 'spring.redis.password', '123456');
 --
 INSERT INTO ms_properties (application, profile, label, key, value)
-VALUES ('account-service', 'dev', 'master', 'spring.kafka.bootstrap-servers', 'localhost:9092');
+VALUES ('market-service', 'dev', 'master', 'spring.kafka.bootstrap-servers', 'localhost:9093');
 INSERT INTO ms_properties (application, profile, label, key, value)
-VALUES ('account-service', 'dev', 'master', 'spring.kafka.consumer.group-id', 'account-service-group');
+VALUES ('market-service', 'dev', 'master', 'spring.kafka.consumer.group-id', 'market-service-group');
